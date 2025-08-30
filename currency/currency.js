c6d8a09f037ceeccabd5a6a82c1d8779f62f64e2
Madiha <madihamubeenhawaldar@gmail.com>
2025-08-30 17:25:11 +0530

    building logic + debugging

--- currency/currency.js
+++ currency/currency.js
@@ -14,7 +14,43 @@ async function loadCurrencies() {
             let option2 = document.createElement("option");
 
             option1.value = option2.value = code;
-            option2.text = option2.text = `${code} - ${symbols[code]}`
+            option2.text = option2.text = `${code} - ${symbols[code].description}`;
+
+            fromSelect.add(option1);
+            toSelect.add(option2);
+            
         }
+
+        //default values
+        fromSelect.value = "USD";
+        toSelect.value = "INR";
+    } catch (error) {
+        resultDiv.innerHTML = " OOPs error occurred while loading currency list !";
     }
 }
+
+async function convertCurrency() {
+    let amount = document.getElementById("amount").value;
+    let from = fromSelect.value;
+    let to = toSelect.value;
+
+    if (!amount || amount <= 0) {
+        resultDiv.innerHTML = "Please do Enter a Valid amount.";
+        return;
+    }
+
+    try {
+        let url = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${amount}`;
+        let res = await fetch(url);
+        let data = await res.json();
+
+        resultDiv.innerHTML = `
+        ${amount} ${from} = <br>
+        <strong>${data.result.toFixed(2)} ${to}</strong>`;
+    } catch (error) {
+        resultDiv.innerHTML = "Conversion failed, please try again later.";
+    }
+}
+
+//Load currency list on page load
+loadCurrencies();
