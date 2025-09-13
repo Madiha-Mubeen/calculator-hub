4a1b33facc464f4d2977d29e2f55964e906cca60
Madiha <madihamubeenhawaldar@gmail.com>
2025-09-13 21:27:14 +0530

    added JavaScript Logic

---
+++ Tip Calculator/tip.js
@@ -0,0 +1,21 @@
+function calculateTip() {
+    const bill = parseFloat(document.getElementById("bill").value);
+    const tipPercent = parseFloat(document.getElementById("tipPercent").value);
+    const people = parseFloat(document.getElementById("people").value);
+
+    if(isNaN(bill) || isNaN(tipPercent) || isNaN(people) || bill <= 0 || tipPercent <= 0 || people <= 0) {
+        document.getElementById("result").innerText = "Please do enter values to calculate the tip";
+        return;
+    }
+
+    const tip = (bill * tipPercent) / 100;
+    const total = bill + tip;
+    const perPerson = total / people;
+
+    document.getElementById("result").innerHtml = `
+    Tipping Amount: $${tip.toFixed(2)} 
+    <br>
+    Total Billing: $${total.toFixed(2)}
+    <br>
+    Per Person Tip: $${perPerson.toFixed(2)}`;
+}
