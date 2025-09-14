b7cd9106ce6fe1cd4266f85f7f28a49b4e35f4dc
Madiha <madihamubeenhawaldar@gmail.com>
2025-09-14 13:09:25 +0530

    added theme toggle script !

--- Percentage/percentage.js
+++ Percentage/percentage.js
@@ -21,3 +21,24 @@ document.getElementById("percentForm").addEventListener("submit", function(e) {
 
     document.getElementById("output").innerText = result;
 });
+
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.toggle("dark");
+    toggleBtn.textContent = "Light Mode";
+
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        toggleBtn.textContent = "Light Mode";
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.textContent = "Dark Mode";
+        localStorage.setItem("theme", "light");
+    }
+});
