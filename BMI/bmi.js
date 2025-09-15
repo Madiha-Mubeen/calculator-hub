8d7ddac3137cf8fbf1100dfd372fd5546ae79e4c
Madiha <madihamubeenhawaldar@gmail.com>
2025-09-15 18:29:22 +0530

    removed unnecessary style & added dark mode style

--- BMI/bmi.js
+++ BMI/bmi.js
@@ -170,19 +170,25 @@ resultDiv.innerHTML = "";
         };
     }
 
-    //Theme toggle 
-    toggleBtn.onclick = () => {
-        document.body.classList.toggle("dark");
-        localStorage.setItem(
-            "theme",
-            document.body.classList.contains("dark") ? "dark" : "light");
-    };
-
-
-    //Apply saved theme on load
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark"
-
-    );
-    }
  }); 
+
+
+const toggleBtn = document.getElementById("themeToggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark");
+    toggleBtn.textContent = "Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        toggleBtn.textContent = "Light Mode";
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.textContent = "Dark Mode";
+        localStorage.setItem("theme", "light");
+    }
+});
