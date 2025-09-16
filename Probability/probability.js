e07a3f304d603ba70d13e28537a8315fe0d369bb
Madiha <madihamubeenhawaldar@gmail.com>
2025-09-16 12:16:30 +0530

    theme toggle script added

--- Probability/probability.js
+++ Probability/probability.js
@@ -8,16 +8,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const resultBox = document.getElementById("result");
     const probOutput = document.getElementById("probOutput");
     const resetBtn = document.getElementById("resetBtn");
-    const themeToggle = document.getElementById("themeToggle");
     const chartCanvas = document.getElementById("probChart");
 
     let chart; //for Chart.js instance
-
-    //Theme toggle 
-    themeToggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark");
-        document.body.classList.toggle("light");
-    });
 
     //Reset Functionality
     resetBtn.addEventListener("click", () => {
@@ -124,4 +117,24 @@ document.addEventListener("DOMContentLoaded", () => {
         
     
  
+});
+
+const toggleBtn  = document.getElementById("themeToggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark");
+    toggleBtn.textContent = "Light Mode";
+}
+
+toggleBtn.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        toggleBtn.textContent = "Light Mode";
+        localStorage.setItem("theme", "dark");
+    } else {
+        toggleBtn.textContent = "Dark Mode";
+        localStorage.setItem("theme", "light");
+    }
 });
